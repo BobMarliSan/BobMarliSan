c05935bc368e4448746447fe312ecf5712cce9fc
BobMarliSan <49477777+BobMarliSan@users.noreply.github.com>
2019-06-13 13:54:38 +0300

    Add files via upload

--- №5.cpp
+++ №5.cpp
@@ -1,29 +1,38 @@
-#include <iostream>
-#include <cstring>
-#include <string.h>
-#include <stdio.h>
-#include <cstdio>
-using namespace std;
 
-char* Func(string str) {
- string s = str;
-	char a[100];
-	for( int i = 0; i < str.length(); i++) {
-    if (str[i] == 'A' || str[i] == 'E' || str[i] == 'O' || str[i] == 'I' || str[i] == 'U' || str[i] == 'Y') {
- str[i] = tolower(str[i]);
+#include <iostream> 
+#include <string> 
+using namespace std;
+void Func(string s, int& a, int& e, int& y, int& u, int& i, int& o) {
+	for (int p = 0; p < s.length(); p++)
+		if (s[p] == ' a') {
+			a++;
 		}
-	}
-	for( int i = 0; i < str.length(); i++) {
- a[i] = str[i];
-	}
-	return a;
+		else if (s[p] == ' e') {
+			e++;
+		}
+		else if (s[p] == ' y') {
+			y++;
+		}
+		else if (s[p] == ' u') {
+			u++;
+		}
+		else if (s[p] == ' i') {
+			i++;
+		}
+		else if (s[p] == ' o') {
+			o++;
+		}
 }
-int main(){
-    if (strcmp(Func("LOREM"), "LoReM") || strcmp(Func("sit"), "sit")|| strcmp(Func(""), "") || strcmp(Func("sit AmeT"), "sit ameT")){
- cout<<"NE OK";
-		return 1;
- } else {
- cout<<"OK";
-	}
+int main() {
+	int a = 0, e = 0, y = 0, u = 0, i = 0, o = 0;
+	string s;
+	getline(cin, s);
+	Func(s, a, e, y, u, i, o);
+	cout << "a = " << a << endl;
+	cout << "e = " << e << endl;
+	cout << "y = " << y << endl;
+	cout << "u = " << u << endl;
+	cout << "i = " << i << endl;
+	cout << "o = " << o << endl;
 	return 0;
 }
